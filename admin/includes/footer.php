d0142f1e954221a731f8c9cba119a4a5dcc5e16d
batsie <46093695+Batsiraik@users.noreply.github.com>
2020-07-10 03:53:33 +0300

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Online Library Management System |<a href="" target="_blank"  > Designed by : Batsirai Kadziya</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
